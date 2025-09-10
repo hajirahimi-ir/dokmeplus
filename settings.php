a7a20822fdcc4b893fd0123fb68d762ca2285eb9
hossein hajirahimi <79391696+hajirahimi-ir@users.noreply.github.com>
2025-09-10 12:28:33 +0330

    Create settings.php

---
+++ settings.php
@@ -0,0 +1,29 @@
+<?php
+if (!defined('ABSPATH')) exit;
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && check_admin_referer('dokmeplus_settings_save')) {
+    update_option('dokmeplus_license', sanitize_text_field($_POST['license_key']));
+    echo '<div class="updated"><p>Settings saved.</p></div>';
+}
+
+$license = get_option('dokmeplus_license', '');
+?>
+
+<div class="wrap">
+    <h1>Dokme Plus Settings</h1>
+    <form method="post">
+        <?php wp_nonce_field('dokmeplus_settings_save'); ?>
+        
+        <table class="form-table">
+            <tr>
+                <th scope="row"><label for="license_key">License Key</label></th>
+                <td>
+                    <input type="text" id="license_key" name="license_key" value="<?php echo esc_attr($license); ?>" class="regular-text" />
+                    <p class="description">Enter your valid license key to unlock full features.</p>
+                </td>
+            </tr>
+        </table>
+
+        <?php submit_button('Save Settings'); ?>
+    </form>
+</div>
