ee476cc77140b1b98384cee53475fb6a66ddd5cf
hossein hajirahimi <79391696+hajirahimi-ir@users.noreply.github.com>
2025-11-17 22:53:22 +0330

    Update dokmeplus.php

--- dokmeplus.php
+++ dokmeplus.php
@@ -15,6 +15,7 @@
 if ( ! defined( 'ABSPATH' ) ) {
     exit;
 }
+require_once __DIR__ . '/elementor.php';
 
 require_once plugin_dir_path(__FILE__) . 'language.php';
 
@@ -560,4 +561,5 @@ add_shortcode( 'dokmeplus', function( $atts ) {
         $text
     );
 } );
+
 
